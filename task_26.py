4dda5994c8f4d6ede0115d2cd95d55756ae92477
laikov <zheka-se@ya.ru>
2021-09-01 12:54:47 +0300

    01-09

---
+++ task_26.py
@@ -0,0 +1,12 @@
+"""
+Define a function param_count that takes a variable number of parameters.
+The function should return the number of arguments it was called with.
+
+For example, param_count() should return 0, while param_count(2, 3, 4) should return 3.
+"""
+
+def param_count(*kwargs):
+    return len(kwargs)
+
+print(param_count(2, 3, 4))
+print(param_count())
